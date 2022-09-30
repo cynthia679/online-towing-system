d182b47ff569472e706985ffaec12dc1a387799f
Cynthia Chebet <cynthiachebet5@gmail.com>
2022-09-30 22:11:46 +0300

    my project

---
+++ resources/views/category/home.blade.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Towing web application</title>
+
+</head>
+<body>
+<div class="content">
+    <div class="title m-b-md">
+        WELCOME TO MY WEBSITE
+        </div>
+    <div class="links">
+        <a href="https://about">About</a>
+        <a href="https://welcome.laravel.com/docs">Welcome</a>
+        <a href="https://home.laravel.com/docs">Homepage</a>
+        <a href="https://dashboard.laravel.com/docs">Dashboard</a>
+        <a href="https://users.laravel.com/docs">Users</a>
+        <a href="https://products.laravel.com/docs">Products</a>
+    </div>
+</div>
+</body>
+</html>
